09060435b3c0083b1c1ef4493b459987967579e3
YuJie Wu <wuvivian725@gmail.com>
2025-04-16 16:58:01 -0700

    Initial Commit

---
+++ SmartBudget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartBudget
+//
+//  Created by YuJie Wu on 2025/4/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
