57361003f707609d5684fdc396fef999419fa8a7
YuJie Wu <wuvivian725@gmail.com>
2025-04-24 15:22:59 -0700

    update .xcodeproj files and init groups page cell and view controller

---
+++ SmartBudget/GroupCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupCell.swift
+//  SmartBudget
+//
+//  Created by YuJie Wu on 2025/4/24.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+    @IBOutlet weak var groupNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SmartBudget/GroupsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GroupsViewController.swift
+//  SmartBudget
+//
+//  Created by YuJie Wu on 2025/4/24.
+//
+
+import UIKit
+
+class GroupsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func didTapAddGroupButton(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
