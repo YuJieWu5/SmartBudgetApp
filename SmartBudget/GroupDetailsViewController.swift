b4df5d162c5db356601f93403cbbd978185a228f
YuJie Wu <wuvivian725@gmail.com>
2025-04-25 11:35:33 -0700

    init groupDetailViewController file

---
+++ SmartBudget/GroupDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GroupDetailsViewController.swift
+//  SmartBudget
+//
+//  Created by YuJie Wu on 2025/4/24.
+//
+
+import UIKit
+
+class GroupDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
